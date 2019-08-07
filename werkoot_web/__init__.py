6049cdfdf8729b21ecec8cf8d34f0849c0fee049
Macintosh <macintosh@Macintoshs-MacBook-Pro.local>
2019-08-07 16:04:29 +0800

    taken out open cv from requi.txt..added blueprints

--- werkoot_web/__init__.py
+++ werkoot_web/__init__.py
@@ -3,6 +3,8 @@ from flask import render_template
 from werkoot_web.blueprints.users.views import users_blueprint
 from werkoot_web.blueprints.sessions.views import sessions_blueprint
 from werkoot_web.blueprints.measurements.views import measurements_blueprint
+from werkoot_web.blueprints.images.views import images_blueprint
+from werkoot_web.blueprints.fan_idol.views import fan_idol_blueprint
 from flask_assets import Environment, Bundle
 from .util.assets import bundles
 
@@ -11,7 +13,9 @@ assets.register(bundles)
 
 app.register_blueprint(users_blueprint, url_prefix="/users")
 app.register_blueprint(sessions_blueprint,url_prefix="/sessions")
+app.register_blueprint(images_blueprint,url_prefix="/images")
 app.register_blueprint(measurements_blueprint, url_prefix="/measurements")
+app.register_blueprint(fan_idol_blueprint,url_prefix="/fan_idol")
 
 @app.errorhandler(500)
 def internal_server_error(e):
---
+++ werkoot_web/blueprints/fan_idol/views.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, render_template,request,url_for,redirect,flash,session
+from werkzeug.security import check_password_hash
+from models.user import User
+from flask_login import login_user,logout_user,current_user
+from models.fan_idol import FanIdol
+
+fan_idol_blueprint = Blueprint('fan_idol',
+                            __name__,
+                            template_folder='templates/fan_idol')
+
+
+@fan_idol_blueprint.route('/follow/<id>', methods=['POST'])
+def follow(id):
+    fan_idol = FanIdol(fan_id = current_user.id, idol_id = id)
+    fan_idol.save()
+    user = User.get_by_id(id)
+    return redirect(url_for('users.show',username = user.username))
+
+@fan_idol_blueprint.route('/unfollow/<id>', methods=['POST'])
+def unfollow(id):
+    unfollow_user = FanIdol.delete().where(FanIdol.fan_id == current_user.id and FanIdol.idol_id == id)
+    unfollow_user.execute()
+    user = User.get_by_id(id)
+    return redirect(url_for('users.show',username = user.username))
+
+
+    
