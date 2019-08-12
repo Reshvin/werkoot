ebb378b80f42e8e3824d5e966979f79045db1885
Macintosh <macintosh@Macintoshs-MacBook-Pro.local>
2019-08-12 17:53:24 +0800

    Edit profile page added

--- werkoot_web/blueprints/users/views.py
+++ werkoot_web/blueprints/users/views.py
@@ -72,16 +72,16 @@ def index():
     return render_template('users/users.html', users = users, text = text)
 
 
-# @users_blueprint.route('/<id>/edit', methods=['GET'])
-# def edit(id):
-#     if not current_user.is_authenticated:
-#         return redirect (url_for('sessions.new'))
-#     else:
-#         user = User.get_by_id(id)
-#         if current_user == user:
-#             return render_template('users/edit.html',user=user)
-#         else:
-#             return redirect (url_for('index'))
+@users_blueprint.route('/<id>/edit', methods=['GET'])
+def edit(id):
+    if not current_user.is_authenticated:
+        return redirect (url_for('sessions.new'))
+    else:
+        user = User.get_by_id(id)
+        if current_user == user:
+            return render_template('users/edit.html',user=user)
+        else:
+            return redirect (url_for('index'))
 
 @users_blueprint.route('/search', methods=['POST'])
 def search():
@@ -98,4 +98,36 @@ def search():
 
 @users_blueprint.route('/<id>', methods=['POST'])
 def update(id):
-    pass
+    old_email = request.form.get('old_email')
+    old_username = request.form.get('old_username')
+    old_password = request.form.get('old_password')
+    new_email = request.form.get('new_email')
+    new_username = request.form.get('new_username')
+    new_password = request.form.get('new_password')
+    confirm_password = request.form.get('confirm_password')
+    new_hashed_password = generate_password_hash(new_password)
+
+    user = User.get_by_id(id)
+
+    if user.username != old_username:
+        flash('Username is not the same')
+        return redirect(url_for('users.new'))
+    elif user.email != old_email:
+        flash('Email is not the same')
+        return redirect(url_for('users.new'))
+    elif new_password != confirm_password:
+        flash('Passwords are not the same')
+        return redirect(url_for('users.new'))
+    if not check_password_hash(user.password, old_password):
+        flash('Incorrect password')
+        return redirect(url_for('users.new'))
+
+    user.username = new_username
+    user.email = new_email
+    user.password = new_hashed_password
+    user.save()
+
+    flash('Profile successfully updated')
+    return redirect(url_for('users.show',username = current_user.username))
+
+
